72ed2265c9da88bb40edd73a1895b10385650dcc
Aswathy1638 <aswathy1638@gmail.com>
2023-07-24 16:10:04 +0530

    Add project files.

---
+++ MiniChatApp2/Data/MiniChatApp2Context.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MiniChatApp2.Model;
+
+namespace MiniChatApp2.Data
+{
+    public class MiniChatApp2Context : DbContext
+    {
+        public MiniChatApp2Context (DbContextOptions<MiniChatApp2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> User { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .Property(u => u.Id)
+                .ValueGeneratedOnAdd();
+
+            modelBuilder.Entity<User>().ToTable("User");
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
